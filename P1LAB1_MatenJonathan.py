11411999f8b003e8be611cdb048468251c60f06d
ironmaten <matenj8877@student.faytechcc.edu>
2024-09-22 18:25:37 -0400

    First submission
    First submission for Module 3

---
+++ P1LAB1_MatenJonathan.py
@@ -0,0 +1,10 @@
+ # Jonathan Maten
+ # 22 September 2024
+ # P1LAB1
+ # This program inputs a first and last name and welcomes them to the class
+
+firstName = input('Enter your first name:')
+lastName = input('Enter your last name:')
+
+print('Hello,', firstName, lastName, end="")
+print('! Welcome to CTI-110')
